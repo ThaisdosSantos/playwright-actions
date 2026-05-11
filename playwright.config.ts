00f12c5c66f47b1e843f3d1deadb7a27faf8491c
ThaisdosSantos <thaisds2.aluno@unipampa.edu.br>
2026-05-11 19:14:15 -0300

    inclusão da url base

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,24 +30,26 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    baseURL: 'https://loginxp.vercel.app'
   },
 
   /* Configure projects for major browsers */
   projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
+    // {
+    //   name: 'chromium',
+    //   use: { ...devices['Desktop Chrome'] },
+    // },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+    
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    
 
     /* Test against mobile viewports. */
     // {
